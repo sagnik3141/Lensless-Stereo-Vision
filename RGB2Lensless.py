102c588ba70d629b894a8994ba87a15ddce3c6cc
Sagnik Ghosh <ee19b132@smail.iitm.ac.in>
2022-04-04 19:06:30 +0530

    Added code to convert single image

--- RGB2Lensless.py
+++ RGB2Lensless.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 from scipy import fft
+import imageio
 
 
 def channel2Lensless(imgC, psfC):
@@ -48,12 +49,49 @@ def RGB2Lensless(img, psf):
     return lenslessRGB
 
 
+def loadCropImg(img_path):
+    """
+    Loads and Crops Image
+    """
+    img = np.array(Image.open(img_path))
+    img = img / 255.0
+    img = cropImg(img)
+
+    return img
+
+
+def cropImg(img):
+    pass
+
+
+def loadPSF(psf_path):
+    """
+    Loads and normalizes PSF
+    """
+    psf = np.load(psf_path)
+    psf = np.stack([psf[:, :, i] / np.sum(psf[:, :, i])
+                   for i in range(3)], axis=2)  # Normalize each channel to 1
+
+    return psf
+
+
 def getArgs():
     pass
 
 
 def main():
-    pass
+
+    args = getArgs()
+
+    if not args.is_dir:
+        img = loadCropImg(args.img_path)
+        psf = loadPSF(args.psf_path)
+
+        lenslessRGB = RGB2Lensless(img, psf)
+        imageio.imwrite(args.save_path, lenslessRGB)
+
+    else:
+        raise NotImplementedError()
 
 
 if __name__ == "__main__":
